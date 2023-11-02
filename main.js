3b506b1fe11c1d438272367ef7c4ae16cab54b70
AndreaBartoloni <andrea.bartoloni00@gmail.com>
2023-11-02 17:49:15 +0100

    secondo commit

--- main.js
+++ main.js
@@ -1,12 +1,25 @@
 pathImg = ["img/01.webp", "img/02.webp", "img/03.webp", "img/04.webp", "img/05.webp" ];
-
+//Inserisco le immagini nel DOM
 let inserireImmagine = "";
 for (let i = 0; i < pathImg.length; i++){
     const percorsoImmagine = pathImg[i];
     inserireImmagine += `<img src="${percorsoImmagine}">`;
 }
 
-document.getElementById("container").innerHTML = inserireImmagine;
+document.getElementById("container-size").innerHTML = inserireImmagine;
 
-let currentImg = 0;
-const images = document.querySelectorAll(".img1", ".img2", ".img3", ".img4", ".img5");
+//Imposto la prima immagine come attiva
+let presentImg = 0;
+const images = document.querySelectorAll("img");
+images[presentImg].classList.add("active");
+
+
+document.getElementById("next").addEventListener("click", function () {
+
+    if(presentImg < images.length - 1){
+        images[presentImg].classList.remove("active");
+        presentImg++;
+        images[presentImg].classList.add("active");
+    }
+
+});
