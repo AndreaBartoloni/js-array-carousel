e8b3119e5237024743e1a4f454db48d08a1cb921
AndreaBartoloni <andrea.bartoloni00@gmail.com>
2023-11-03 14:09:06 +0100

    last commit

--- main.js
+++ main.js
@@ -1,16 +1,16 @@
 pathImg = ["img/01.webp", "img/02.webp", "img/03.webp", "img/04.webp", "img/05.webp" ];
-//Inserisco le immagini nel DOM
+// // //Inserisco le immagini nel DOM
 let inserireImmagine = "";
 for (let i = 0; i < pathImg.length; i++){
     const percorsoImmagine = pathImg[i];
     inserireImmagine += `<img src="${percorsoImmagine}">`;
 }
 
-document.getElementById("container-size").innerHTML = inserireImmagine;
+document.getElementById("container").innerHTML += inserireImmagine;
 
 //Imposto la prima immagine come attiva
 let presentImg = 0;
-const images = document.querySelectorAll("img");
+const images = document.querySelectorAll("#container img");
 images[presentImg].classList.add("active");
 
 
@@ -19,6 +19,15 @@ document.getElementById("next").addEventListener("click", function () {
     if(presentImg < images.length - 1){
         images[presentImg].classList.remove("active");
         presentImg++;
+        images[presentImg].classList.add("active");
+    }
+
+});
+document.getElementById("prev").addEventListener("click", function () {
+
+    if(presentImg > 0){
+        images[presentImg].classList.remove("active");
+        presentImg--;
         images[presentImg].classList.add("active");
     }
 
